af8d79c6b3feb8797a5cdd28f97aabf5c8036bca
yyagiz-h <hakan4578sen@gmail.com>
2026-03-04 10:51:13 +0300

    Ders-3 Örnekler

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int k = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            k += 1;
+            this.Text = string.Format("k={0}", k);
+            if (k > 200)
+            {
+                this.Width = k;
+                this.Height = k;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled == true)
+            {
+                button1.Text = "durdur";
+            }
+            else 
+            { 
+                button1.Text= "çalıştır ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        double [] sayi=new double [1000];
+        int k = 0;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("BİR SAYI GİRİN");
+                return;
+            }
+            sayi[k]= Convert.ToInt32(textBox1.Text);
+            listBox1.Items.Add((k + 1).ToString() + ". sayı=" + sayi[k].ToString());
+            k += 1;
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double toplam= 0 , ort= 0 ;
+            for (int i = 0; i < k; i++)
+            {
+                toplam = sayi[i];
+            }
+            ort = toplam / Convert.ToDouble(k);
+            this.Text = toplam.ToString();
+            this.Text = String.Format("toplam={0} ve ortalama={1}",toplam,ort);
+
+        }
+    }
+}
